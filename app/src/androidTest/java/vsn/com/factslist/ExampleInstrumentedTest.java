c5403812474a7287fb0d1056a9306c8f74b8e198
nagendra <vsnbabu12@gmail.com>
2018-08-06 08:20:40 +0530

    #UI polish

--- app/src/androidTest/java/vsn/com/factslist/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/vsn/com/factslist/ExampleInstrumentedTest.java
@@ -1,9 +1,15 @@
 package vsn.com.factslist;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,6 +20,8 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+//@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.JELLY_BEAN)
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
